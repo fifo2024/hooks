1bc29a7122fe1cd99608be9a9031cd3e4bee6660
zhaoyadong <zhaoyadong@baidu.com>
2024-10-18 10:08:27 +0800

    add fifo20204 hooks and publish

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @file entry file
+ * @author zhifou
+ */
+export { default as useLocalStorage } from "./useLocalStorage";
+export { default as useWindowSize } from "./useWindowSize";
+export { default as useOnGlobalEvent } from "./useOnGlobalEvent";
+export { default as useNavigatorOnLine } from "./useNavigatorOnLine";
+export { default as usePrevious } from "./usePrevious";
+export { default as useRerender } from "./useRerender";
